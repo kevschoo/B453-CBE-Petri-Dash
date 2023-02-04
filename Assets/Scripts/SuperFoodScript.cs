896e04a7690ba3f5bc780462bd43e830d6e1ae5f
Leo <c00250593@itcarlow.ie>
2023-02-04 17:49:47 +0000

    Start of super food prefab

---
+++ Assets/Scripts/SuperFoodScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperFoodScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //Do something with traits
+    void Start()
+    {
+        
+    }
+}
