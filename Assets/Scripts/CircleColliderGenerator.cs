3a9c7fe19bcd0e1dc4e9e059dcabc6bd0293a2d4
Leo <c00250593@itcarlow.ie>
2023-02-04 15:42:07 +0000

    More work on the player itself

---
+++ Assets/Scripts/CircleColliderGenerator.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleColliderGenerator : MonoBehaviour
+{
+
+    void Awake()
+    {
+        PolygonCollider2D poly = GetComponent<PolygonCollider2D>();
+        if (poly == null)
+        {
+            poly = gameObject.AddComponent<PolygonCollider2D>();
+        }
+        Vector2[] points = poly.points;
+        EdgeCollider2D edge = gameObject.AddComponent<EdgeCollider2D>();
+        edge.points = points;
+        Destroy(poly);
+    }
+
+}
--- Assets/Scripts/FoodScript.cs
+++ Assets/Scripts/FoodScript.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class FoodScript : MonoBehaviour
 {
     public int m_foodAmount;
+    
     void Start()
     {
         m_foodAmount = Random.Range(1,10);
+        transform.localScale = new Vector3( m_foodAmount, m_foodAmount);
     }
     
-
+    public void UpdateFoodAmount()
+    {
+        transform.localScale = new Vector3(m_foodAmount, m_foodAmount);
+    }
 }
